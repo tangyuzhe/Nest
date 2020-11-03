22cf80ecddc18816cf8efaae0d4068abab5ed4d4
tangyuzhe <www.twuwu@qq.com>
2020-11-03 17:34:59 +0800

    mysql连接测试

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function index() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const options = new DocumentBuilder()
-    .setTitle('nest入门接口标题')
-    .setDescription('使用nest书写的常用性接口') // 文档介绍
+    .setTitle('毕设接口文档')
+    .setDescription('Swagger接口文档') // 文档介绍
     .setVersion('1.0.0') // 文档版本
-    .addTag('用户,安全') // 每个tag标签都可以对应着几个@ApiUseTags('用户,安全') 然后被ApiUseTags注释，字符串一致的都会变成同一个标签下的
     // .setBasePath('http://localhost:5000')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/api', app, document);
+  SwaggerModule.setup('/v1/api', app, document);
   await app.listen(5000);
 }
-bootstrap();
+index();
